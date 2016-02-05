949c8cb88d4162f8b48387b3a54e3c305683709f
barriquello <barriquello@gmail.com>
2016-02-05 20:31:24 -0200

    correct makefiles relative path

--- eclipse_gcc_mcf51qe128/Debug/Sources_brtos/subdir.mk
+++ eclipse_gcc_mcf51qe128/Debug/Sources_brtos/subdir.mk
@@ -4,14 +4,14 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../../../brtos/brtos/BRTOS.c \
-../../../brtos/brtos/OSInfo.c \
-../../../brtos/brtos/OSTime.c \
-../../../brtos/brtos/mbox.c \
-../../../brtos/brtos/mutex.c \
-../../../brtos/brtos/queue.c \
-../../../brtos/brtos/semaphore.c \
-../../../brtos/brtos/stimer.c 
+../../brtos/brtos/BRTOS.c \
+../../brtos/brtos/OSInfo.c \
+../../brtos/brtos/OSTime.c \
+../../brtos/brtos/mbox.c \
+../../brtos/brtos/mutex.c \
+../../brtos/brtos/queue.c \
+../../brtos/brtos/semaphore.c \
+../../brtos/brtos/stimer.c 
 
 OBJS += \
 ./Sources_brtos/BRTOS.o \
@@ -35,56 +35,56 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Sources_brtos/BRTOS.o: ../../../brtos/brtos/BRTOS.c
+Sources_brtos/BRTOS.o: ../../brtos/brtos/BRTOS.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Coldfire C Compiler'
 	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I"../../eclipse_gcc_mcf51qe128/Sources" -I"../../eclipse_gcc_mcf51qe128/Project_Headers" -I"../../eclipse_gcc_mcf51qe128/../brtos/brtos/includes" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/GCC_CFV1" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/MemoryAllocation" -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
 	@echo 'Finished building: $<'
 	@echo ' '
 
-Sources_brtos/OSInfo.o: ../../../brtos/brtos/OSInfo.c
+Sources_brtos/OSInfo.o: ../../brtos/brtos/OSInfo.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Coldfire C Compiler'
 	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I"../../eclipse_gcc_mcf51qe128/Sources" -I"../../eclipse_gcc_mcf51qe128/Project_Headers" -I"../../eclipse_gcc_mcf51qe128/../brtos/brtos/includes" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/GCC_CFV1" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/MemoryAllocation" -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
 	@echo 'Finished building: $<'
 	@echo ' '
 
-Sources_brtos/OSTime.o: ../../../brtos/brtos/OSTime.c
+Sources_brtos/OSTime.o: ../../brtos/brtos/OSTime.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Coldfire C Compiler'
 	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I"../../eclipse_gcc_mcf51qe128/Sources" -I"../../eclipse_gcc_mcf51qe128/Project_Headers" -I"../../eclipse_gcc_mcf51qe128/../brtos/brtos/includes" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/GCC_CFV1" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/MemoryAllocation" -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
 	@echo 'Finished building: $<'
 	@echo ' '
 
-Sources_brtos/mbox.o: ../../../brtos/brtos/mbox.c
+Sources_brtos/mbox.o: ../../brtos/brtos/mbox.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Coldfire C Compiler'
 	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I"../../eclipse_gcc_mcf51qe128/Sources" -I"../../eclipse_gcc_mcf51qe128/Project_Headers" -I"../../eclipse_gcc_mcf51qe128/../brtos/brtos/includes" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/GCC_CFV1" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/MemoryAllocation" -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
 	@echo 'Finished building: $<'
 	@echo ' '
 
-Sources_brtos/mutex.o: ../../../brtos/brtos/mutex.c
+Sources_brtos/mutex.o: ../../brtos/brtos/mutex.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Coldfire C Compiler'
 	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I"../../eclipse_gcc_mcf51qe128/Sources" -I"../../eclipse_gcc_mcf51qe128/Project_Headers" -I"../../eclipse_gcc_mcf51qe128/../brtos/brtos/includes" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/GCC_CFV1" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/MemoryAllocation" -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
 	@echo 'Finished building: $<'
 	@echo ' '
 
-Sources_brtos/queue.o: ../../../brtos/brtos/queue.c
+Sources_brtos/queue.o: ../../brtos/brtos/queue.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Coldfire C Compiler'
 	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I"../../eclipse_gcc_mcf51qe128/Sources" -I"../../eclipse_gcc_mcf51qe128/Project_Headers" -I"../../eclipse_gcc_mcf51qe128/../brtos/brtos/includes" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/GCC_CFV1" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/MemoryAllocation" -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
 	@echo 'Finished building: $<'
 	@echo ' '
 
-Sources_brtos/semaphore.o: ../../../brtos/brtos/semaphore.c
+Sources_brtos/semaphore.o: ../../brtos/brtos/semaphore.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Coldfire C Compiler'
 	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I"../../eclipse_gcc_mcf51qe128/Sources" -I"../../eclipse_gcc_mcf51qe128/Project_Headers" -I"../../eclipse_gcc_mcf51qe128/../brtos/brtos/includes" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/GCC_CFV1" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/MemoryAllocation" -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
 	@echo 'Finished building: $<'
 	@echo ' '
 
-Sources_brtos/stimer.o: ../../../brtos/brtos/stimer.c
+Sources_brtos/stimer.o: ../../brtos/brtos/stimer.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Coldfire C Compiler'
 	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I"../../eclipse_gcc_mcf51qe128/Sources" -I"../../eclipse_gcc_mcf51qe128/Project_Headers" -I"../../eclipse_gcc_mcf51qe128/../brtos/brtos/includes" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/GCC_CFV1" -I"../../eclipse_gcc_mcf51qe128/../brtos/hal/MemoryAllocation" -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
