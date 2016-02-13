8c5072e3eaaa1a1714f96f3081301a3366aad442
barriquello <barriquello@gmail.com>
2016-02-13 16:11:05 -0200

    correct project paths

--- platforms/atmelstudio_gcc_avr/brtos_atmega2560/Debug/makedep.mk
+++ platforms/atmelstudio_gcc_avr/brtos_atmega2560/Debug/makedep.mk
@@ -2,21 +2,21 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-..\..\brtos\brtos\BRTOS.c
+..\..\..\brtos\brtos\BRTOS.c
 
-..\..\brtos\brtos\mbox.c
+..\..\..\brtos\brtos\mbox.c
 
-..\..\brtos\brtos\mutex.c
+..\..\..\brtos\brtos\mutex.c
 
-..\..\brtos\brtos\OSTime.c
+..\..\..\brtos\brtos\OSTime.c
 
-..\..\brtos\brtos\queue.c
+..\..\..\brtos\brtos\queue.c
 
-..\..\brtos\brtos\semaphore.c
+..\..\..\brtos\brtos\semaphore.c
 
-..\..\brtos\brtos\stimer.c
+..\..\..\brtos\brtos\stimer.c
 
-..\..\brtos\hal\Atmel_Studio_6_ATMEGA\HAL.c
+..\..\..\brtos\hal\Atmel_Studio_6_ATMEGA\HAL.c
 
 ..\debug_info.c
 
