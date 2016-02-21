fb18e5809f0184f5bb785227959194a43bda18db
barriquello <barriquello@gmail.com>
2016-02-21 13:00:52 -0300

    update mk files

--- platforms/eclipse_gcc_mcf51qe128/Debug/hal/subdir.mk
+++ platforms/eclipse_gcc_mcf51qe128/Debug/hal/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 hal/HAL.o: ../../../brtos/hal/GCC_CFV1/HAL.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Coldfire C Compiler'
-	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I"../../../platforms/eclipse_gcc_mcf51qe128/Project_Headers" -I"../../../platforms/eclipse_gcc_mcf51qe128/../../brtos/brtos/includes" -I"../../../platforms/eclipse_gcc_mcf51qe128/Sources" -I"../../../platforms/eclipse_gcc_mcf51qe128/../../brtos/hal/GCC_CFV1" -I"../../../platforms/eclipse_gcc_mcf51qe128/../../brtos/hal/MemoryAllocation" -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
+	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I../Project_Headers -I../Sources -I../../../brtos/brtos/includes -I../../../brtos/hal/MemoryAllocation -I../../../brtos/hal/GCC_CFV1 -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
 	@echo 'Finished building: $<'
 	@echo ' '
 
